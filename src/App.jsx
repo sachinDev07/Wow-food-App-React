73892c561908192dba2d6b957e51fc19db91c144
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-10 14:24:27 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-red-700'>Hello World!</h1>
+    </>
+  )
+}
+
+export default App
