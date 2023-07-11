e2f6b2f28668eb666b1b95449d2028d8afacdf0d
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-11 11:10:55 +0500

    Created component for the lazy loading

---
+++ src/components/Img.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
+const Img = ({ src, className }) => {
+  return (
+    <div>
+      <LazyLoadImage
+        className={className || ""}
+        alt="background-img"
+        effect="blur"
+        src={src}
+      />
+    </div>
+  );
+};
+
+export default Img;
