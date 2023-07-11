949f021518e714a9bacb49be61d08c06dbf089ab
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-11 11:15:01 +0500

    Added App api and img cdn api

---
+++ src/utils/constant.js
@@ -0,0 +1,5 @@
+export const APP_API =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.0363768&lng=72.5466727&sortBy=RELEVANCE&page_type=DESKTOP_WEB_LISTING";
+
+export const IMG_CDN_API =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/";
