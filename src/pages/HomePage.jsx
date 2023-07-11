d241a58cc24e82c2f2e50276c891a22375f6db46
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-11 11:12:29 +0500

    added homepage for the app

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Slider from '../components/Slider'
+
+const HomePage = () => {
+  return (
+    <div className='bg-white'>
+        <Slider />
+    </div>
+  )
+}
+
+export default HomePage
