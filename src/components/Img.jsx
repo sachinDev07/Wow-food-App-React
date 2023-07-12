865384c9aaa73f82596e765fa9767f5776eaea5c
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-12 22:00:10 +0500

    Added for LazyLoading of image

--- src/components/Img.jsx
+++ src/components/Img.jsx
@@ -4,14 +4,12 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 
 const Img = ({ src, className }) => {
   return (
-    <div>
-      <LazyLoadImage
-        className={className || ""}
-        alt="background-img"
-        effect="blur"
-        src={src}
-      />
-    </div>
+    <LazyLoadImage
+      className={className}
+      alt="background-img"
+      effect="blur"
+      src={src}
+    />
   );
 };
 
