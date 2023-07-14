f2ef318db47e605fd17cfb2045255ab9daba828a
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-14 19:34:45 +0500

    Added functionality that shows how many items are present in the cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const cartItems = useSelector((store) => store.cart.items);
   return (
     <header className="fixed left-0 right-0 bg-white z-50 px-44 shadow-lg">
       <nav className="w-full h-[60px] flex items-center justify-between">
@@ -19,9 +21,10 @@ const Header = () => {
           <li className="text-lg font-medium hover:text-orange-500">
             <Link to="/singIn">Sign In</Link>
           </li>
-          <li>
+          <li className="relative">
             <Link to="/cart">
               <i className="fa-solid fa-cart-shopping fa-lg hover:text-orange-500"></i>
+              <span className="absolute -top-2 right-0 left-3 text-white text-[11px] w-4 h-4 flex items-center justify-center rounded-full bg-blue-500">{cartItems.length}</span>
             </Link>
           </li>
         </ul>
