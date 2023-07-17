33842a4673352a1cbde7ddf38cb56cfd1f76734b
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-17 15:54:08 +0500

    Search page is done

---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import SearchItem from '../components/SearchItem'
+
+const SearchPage = () => {
+  return (
+    <div className='pt-[100px] px-44'>
+        <SearchItem />
+    </div>
+  )
+}
+
+export default SearchPage
