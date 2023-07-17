8f2691831cdb4693d032cde44b63b0551a7b44fb
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-17 12:22:26 +0500

    Refactor component logic to custome hook

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,54 +1,10 @@
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import RestaurantCard from "./RestaurantCard";
+import useGetRestaurantLists from "../utils/useGetRestaurantLists";
 
 const RestaurantMenu = () => {
-  const [restaurantData, setRestaurantData] = useState(null);
-  const [numberOfRestaurants, setNumberOfRestaurants] = useState(null);
-  const [sortsData, setSortsData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [filterApi, setFilterApi] = useState("RELEVANCE");
-  const [filterCount, setFilterCount] = useState(0);
-
-  useEffect(() => {
-    getRestaurantLists();
-  }, [filterApi]);
-
-  const getRestaurantLists = async () => {
-    const api = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.0363768&lng=72.5466727&sortBy=${filterApi}&page_type=DESKTOP_WEB_LISTING&`;
-    try {
-      const response = await fetch(api);
-      const responseData = await response.json();
-
-      if (filterCount === 0) {
-        setNumberOfRestaurants(
-          responseData.data.cards[2].data.data.totalOpenRestaurants
-        );
-        setSortsData(responseData.data.sorts);
-        setRestaurantData(responseData.data.cards[2].data.data);
-      } else {
-        setNumberOfRestaurants(
-          responseData.data.cards[0].data.data.totalRestaurants
-        );
-        setRestaurantData(responseData.data.cards[0].data.data);
-      }
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const handleApiFilter = (item) => {
-    if (item.defaultSelection) {
-      setFilterCount(0);
-      setFilterApi(item.key);
-    } else {
-      setFilterCount(filterCount + 1);
-      setFilterApi(item.key);
-    }
-  };
+  const { restaurantData, numberOfRestaurants, sortsData, loading, error } =
+    useGetRestaurantLists();
 
   if (loading) {
     return <div>Loading...</div>;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,28 +4,11 @@ import "react-multi-carousel/lib/styles.css";
 
 import { responsive } from "../utils/breakPoints";
 import Img from "./Img";
-import { APP_API, IMG_CDN_API } from "../utils/constant";
+import { IMG_CDN_API } from "../utils/constant";
+import useFetchApi from "../utils/useFetchApi";
 
 const Slider = () => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetchApi();
-  }, []);
-
-  const fetchApi = async () => {
-    try {
-      const response = await fetch(APP_API);
-      const data = await response.json();
-      setData(data.data.cards[0].data.data.cards);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const { data, loading, error } = useFetchApi();
 
   if (loading) {
     return <div>Loading...</div>;
@@ -39,7 +22,10 @@ const Slider = () => {
     <div className="px-44 bg-slate-900 py-12 relative z-30">
       <Carousel responsive={responsive}>
         {data?.map((myitems) => (
-          <div key={IMG_CDN_API + myitems.data.bannerId} className="transition-transfrom hover:scale-105 duration-150">
+          <div
+            key={IMG_CDN_API + myitems.data.bannerId}
+            className="transition-transfrom hover:scale-105 duration-150"
+          >
             <Img
               className="h-auto w-64 "
               src={IMG_CDN_API + myitems.data.creativeId}
