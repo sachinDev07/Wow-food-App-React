aeb24614b0330b464171a8066b63936c15b8b3f9
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-17 15:53:47 +0500

    added code

---
+++ src/components/SearchSuggestions.jsx
@@ -0,0 +1,18 @@
+import { IMG_CDN_API } from "../utils/constant";
+import Img from "./Img";
+
+const SearchSuggestions = ({ text, type, cloudinaryId }) => {
+  return (
+    <div className="ml-8 h-full cursor-pointer hover:bg-sky-50 py-4">
+      <div className="flex items-center gap-6">
+        <Img src={IMG_CDN_API + cloudinaryId} className="w-16 h-16" />
+        <div className="flex flex-col gap-1">
+          <span className="text-base text-slate-900">{text}</span>
+          <span className=" text-slate-700">{type}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SearchSuggestions;
