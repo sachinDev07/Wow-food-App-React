fc73263dbc3be709289d3a30c0b897405b33bf83
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-19 15:49:22 +0500

    created store for the loginslice

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cartSlice";
+import loginSlice from "./loginSlice";
 
 
 const store = configureStore({
     reducer: {
         cart: cartSlice,
+        login: loginSlice,
     },
 })
 
