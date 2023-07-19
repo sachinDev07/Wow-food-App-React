b979f4e240f87dfec19554ebb9de05de49d40a9d
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-19 16:38:21 +0500

    little bit styel change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="flex items-center justify-center h-full bg-gray-800 text-white py-4 px-6 sm:px-12">
       <div className="container mx-auto flex items-center justify-center">
         <p className="text-sm">
-          &copy; Made with React ♥
+          &copy; Made with React <span className="text-orange-400"> ♥ </span> by <span className="text-orange-400">Sachin</span>
         </p>
       </div>
     </footer>
