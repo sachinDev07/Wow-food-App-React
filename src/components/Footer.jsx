af8602521c758b53f78f9e4e423bffb70303a9e8
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-19 15:46:40 +0500

    footer done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="flex items-center justify-center h-full bg-gray-800 text-white py-4 px-6 sm:px-12">
+      <div className="container mx-auto flex items-center justify-center">
+        <p className="text-sm">
+          &copy; Made with React ♥
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
