e3d05f92a21faad6c2dd1bac62457cb1108b1118
Sachin kumar <110657130+Sachin-RJ7@users.noreply.github.com>
2023-07-19 15:42:54 +0500

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom";
 
-import Header from "./components/Header";
 import { Provider } from "react-redux";
 import store from "./utils/store";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Provider store={store}>
       <Header />
       <Outlet />
+      <Footer />
     </Provider>
   );
 }
